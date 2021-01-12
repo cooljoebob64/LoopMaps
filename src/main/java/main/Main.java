7463a046fdbc90916478dd72ad926feef471f3ec
Joshua Luppes <cooljoebob64@gmail.com>
2021-01-12 14:47:01 -0600

    Initial commit. Adds Number Cruncher and launcher app.

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner kb = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean goodInput = false;
+        String userResponse = "";
+
+        System.out.println("Welcome to the Loop Mapping Exercise!");
+        pressEnter();
+
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            int choice = 0;
+            goodInput=false;
+            while (!goodInput) {
+                System.out.println("Which program would you like to run?");
+                System.out.println("1. Number Cruncher");
+                System.out.println("2. Car Dealer");
+                System.out.println("Q. Quit");
+                userResponse = kb.nextLine();
+
+                try {
+                    choice = Integer.parseInt(userResponse);
+                    goodInput = true;
+                } catch (NumberFormatException e) {
+                    if (userResponse.contains("q")) {
+                        goodInput = true;
+                        keepGoing = false;
+                        System.out.println("Quitting the program!");
+                    } else {
+                        System.out.println("Received error: " + e);
+                        System.out.println("Invalid entry! Try again...");
+                        goodInput = false;
+                    }
+                }
+
+            }
+            switch (choice) {
+                case 0 -> System.out.println("Goodbye!");
+                case 1 -> launchNumberCruncher();
+                case 2 -> launchCarDealer();
+                default -> System.out.println("Something went wrong! No valid option selected! You said: " + userResponse);
+            }
+
+
+        }
+
+        System.out.println("Program complete!");
+    }
+
+    private static void launchNumberCruncher(){
+        System.out.println("Launching Number Cruncher!");
+        pressEnter();
+        new NumberCruncher();
+    }
+
+    private static void launchCarDealer(){
+        System.out.println("Launching Car Dealer!");
+        pressEnter();
+
+        System.out.println("Car dealer not implemented yet! Check back soon!");
+        pressEnter();
+    }
+
+    private static void pressEnter(){
+        System.out.println("Press Enter to continue...");
+        kb.nextLine();
+    }
+}
---
+++ src/main/java/main/NumberCruncher.java
@@ -0,0 +1,111 @@
+package main;
+
+import java.util.Scanner;
+
+public class NumberCruncher {
+
+    private final static int DEFAULT_SIZE = 5;
+    private double[] numberList;
+    private final Scanner kb = new Scanner(System.in);
+
+    public NumberCruncher(int size) {
+        newList(size);
+        displayIntro();
+        getNumbers();
+        displayList();
+        displayCalcs();
+    }
+
+    public NumberCruncher() {
+        this(DEFAULT_SIZE);
+    }
+
+    private void newList(int size) {
+        numberList = new double[size];
+    }
+
+    private void displayIntro() {
+        System.out.println("Welcome to Number Cruncher!");
+        System.out.println("I'm going to ask you for " + numberList.length + " numbers, and then we'll analyze them!");
+    }
+
+    private double getSum() {
+        double sum = 0;
+        for (double num : numberList)
+            sum += num;
+        return sum;
+    }
+
+    private double getProduct() {
+        double product = 1;
+        for (double num : numberList
+        ) {
+            product *= num;
+        }
+        return product;
+    }
+
+    private double getLargest() {
+        double largest = numberList[0];
+        for (double num : numberList
+        ) {
+            if (num > largest) largest = num;
+        }
+        return largest;
+    }
+
+    private double getSmallest() {
+        double smallest = numberList[0];
+        for (double num : numberList
+        ) {
+            if (num < smallest) smallest = num;
+        }
+        return smallest;
+    }
+
+    private void getNumbers() {
+        for (int i = 0; i < numberList.length; i++) {
+            double userEntry = 0.666;
+            boolean entryClean = false;
+            while (!entryClean) {
+                System.out.print("Please enter number #" + (i + 1) + ": ");
+                try {
+                    userEntry = Double.parseDouble(kb.nextLine());
+                    entryClean = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Uh oh, got the error: " + e);
+                    System.out.println("That's not a valid entry! Try again...");
+                    entryClean = false;
+                }
+            }
+            System.out.println("Got it! Adding " + userEntry + " to the list!");
+            numberList[i] = userEntry;
+        }
+    }
+
+    private void displayList() {
+        System.out.println("Our number list: ");
+        System.out.println(listBuilder(numberList));
+    }
+
+    private String listBuilder(double[] list) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < list.length; i++
+        ) {
+            builder.append(list[i]);
+            if (i < list.length - 1) {
+                builder.append(", ");
+            }
+        }
+        return builder.toString();
+    }
+
+    private void displayCalcs(){
+        System.out.println("--- Our calculations ---");
+        System.out.println("Sum of all numbers: " + getSum());
+        System.out.println("Product of all numbers: " + getProduct());
+        System.out.println("Largest of all numbers: " + getLargest());
+        System.out.println("Smallest of all numbers: " + getSmallest());
+    }
+
+}
